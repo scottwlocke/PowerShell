acefed4a9d939fc4335b307fb1816640826d69b5
Scott W Locke <Scott.W.Locke@gmail.com>
2019-01-27 18:55:30 -0500

    Added MSIEXEC hashtable example

--- HashTableExample.ps1
+++ HashTableExample.ps1
@@ -6,3 +6,19 @@ $hash = @{}
 $processes = get-process | Sort-Object -Property name -Unique
 # Add process name and PID to table
 foreach ($process in $processes) { $hash.add($process.name, $process.id)}
+
+# Useful for when a lot of paramters need to be used and readability suffers
+
+$InstallerArgs = @{
+    FilePath = 'msiexec.exe'
+    ArgumentList = @(
+        '/i',
+        'C:\Files\Installer.msi',
+        "/qb!",
+        "/norestart",
+        "/l*v",
+        '"C:\Temp\alkane_install_log.log"'
+    )
+    Wait = $True
+}
+Start-Process @InstallerArgs
