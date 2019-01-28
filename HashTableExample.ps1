5da979717d325c3137231a873c3d4dc677f6b25a
Scott W Locke <Scott.W.Locke@gmail.com>
2019-01-27 19:02:49 -0500

    Added more settings to MSIEXEC hashtable example

--- HashTableExample.ps1
+++ HashTableExample.ps1
@@ -8,7 +8,8 @@ $processes = get-process | Sort-Object -Property name -Unique
 foreach ($process in $processes) { $hash.add($process.name, $process.id)}
 
 # Useful for when a lot of paramters need to be used and readability suffers
-
+# Run msiexec as an admin minimized, wait for completion specify working dir
+# Pass ArguementLIst parameters to the MSI specified
 $InstallerArgs = @{
     FilePath = 'msiexec.exe'
     ArgumentList = @(
@@ -20,5 +21,8 @@ $InstallerArgs = @{
         '"C:\Temp\Application_install_log.log"'
     )
     Wait = $True
+    WorkingDirectory = "C:\Temp"
+    WindowStyle = "Minimized"
+    Verb = "RunAs"
 }
 Start-Process @InstallerArgs
