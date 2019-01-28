0fa00c88a1c87008fdf177329990a6bd3d1ec6bd
Scott W Locke <Scott.W.Locke@gmail.com>
2019-01-27 20:12:52 -0500

    Added Simple IsAdmin oneliner Variable check

---
+++ IsAdmin.ps1
@@ -0,0 +1,3 @@
+# Create Variable $IsAdmin which will be $True if admin $False if not
+
+$IsAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
