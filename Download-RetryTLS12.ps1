0a0013d15e775439957e58def2fba3715914c7a1
Scott W Locke <Scott.W.Locke@gmail.com>
2019-01-28 22:11:19 -0500

    Quick Function to try and download, set PS to TLS1.2 if fails

---
+++ Download-RetryTLS12.ps1
@@ -0,0 +1,14 @@
+# Downloading a file, retrying if TLS 1.2 is required
+
+Try {
+    Invoke-WebRequest -Uri 'https://somewebsite.com/somfile.zip' -OutFile $env:userprofile\Downloads\somefile.zip
+}
+Catch [System.Net.WebException] {
+    # SecurityProtocol for the local PowerShell session will be updated to access TLS 1.2 and try download again.
+    $secProtocol = [Net.ServicePointManager]::SecurityProtocol
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest -Uri 'https://somewebsite.com/somfile.zip' -OutFile $env:userprofile\Downloads\somefile.zip
+    # Be nice set it back to waht it was before we changed it
+    [Net.ServicePointManager]::SecurityProtocol = $secProtocol
+}
+ 
