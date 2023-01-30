83d4060cc59206c60b600761a95a578d7a2271b0
Scott Locke <scott.w.locke@gmail.com>
2023-01-30 17:31:20 -0500

    Added Merge-Module function, work on building single psm1 file from multiple PS1 files

---
+++ Merge-Module.ps1
@@ -0,0 +1,24 @@
+# Merge's multiple PS1 files into a single PSM1 file
+# This greatly reduces load time of the module
+# Keeping the cmdlets in individual PS1 files aids in troubleshooting
+function Merge-Module {
+    param (
+        [string] $ModuleName,
+        [string] $ModulePathSource,
+        [string] $ModulePathTarget
+    )
+    $ScriptFunctions = @( Get-ChildItem -Path $ModulePathSource\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+    $ModulePSM = @( Get-ChildItem -Path $ModulePathSource\*.psm1 -ErrorAction SilentlyContinue -Recurse )
+
+    foreach ($FilePath in $ScriptFunctions) {
+        $Results = [System.Management.Automation.Language.Parser]::ParseFile($FilePath, [ref]$null, [ref]$null)
+        $Functions = $Results.EndBlock.Extent.Text
+        $Functions | Add-Content -Path "$ModulePathTarget\$ModuleName.psm1"
+    }
+
+    foreach ($FilePath in $ModulePSM) {
+        $Content = Get-Content $FilePath
+        $Content | Add-Content -Path "$ModulePathTarget\$ModuleName.psm1"
+    }
+    Copy-Item -Path "$ModulePathSource\$ModuleName.psd1" "$ModulePathTarget\$ModuleName.psd1"
+}
